52fd0122a2d5a427bcd9251da4fde9abef2ffbc9
H B <phajib@gmail.com>
2019-02-04 08:52:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,19 +15,14 @@ end
 
 def apply_coupons(cart, coupons)
   return cart if coupons == []
-#set new_cart = cart so we don't have to push all the values, just change them
   cart_update = cart
   coupons.each do |coupon|
-    name = coupon[:item] #avocado, cheese,...
+    name = coupon[:item]
     no_coupons = coupon[:num]
-  #if the cart has the same item in coupon and has larger amount than in coupon
     if  cart.include?(name) && cart[name][:count] >= no_coupons
-     #remove number of the new_cart's count
      cart_update[name][:count] -= no_coupons
-     #increase the count when there is more items than the coupon allows
      if cart_update["#{name} W/COUPON"]
        cart_update["#{name} W/COUPON"][:count] += 1
-     #set the name with coupon with new value
      else
        cart_update["#{name} W/COUPON"] = {
          :price => coupon[:cost],
